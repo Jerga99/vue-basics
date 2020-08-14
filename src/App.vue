795269a3fc3cd9c7c5ddfa1b98c90d16533017ed
Filip Jerga <jerga99@gmail.com>
2020-08-14 10:51:14 +0200

    Counter

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,35 @@
 
 <template>
   <div class="my-app">
-    <h1>Hello World</h1>
+    <h1>{{counter}}</h1>
+    <button v-on:click="increment(1)">Increment</button>
+    <button v-on:click="increment(-1)">Decrement</button>
   </div>
 </template>
 
 <script>
   export default {
-
+    data() {
+      return {
+        counter: 1000
+      }
+    },
+    methods: {
+      increment(inc) {
+        this.counter += inc
+      }
+    }
   }
 </script>
 
 <style>
   .my-app {
     color: blue;
+    padding: 200px;
+  }
+
+  h1 {
+    font-size: 70px;
   }
 
 </style>
