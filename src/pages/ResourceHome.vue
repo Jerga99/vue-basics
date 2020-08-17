d446b608648e9687181972abd773da8bc50c3c19
Filip Jerga <jerga99@gmail.com>
2020-08-17 10:16:27 +0200

    Add resource

--- src/pages/ResourceHome.vue
+++ src/pages/ResourceHome.vue
@@ -9,6 +9,9 @@
         </h4>
         <resource-search />
         <resource-list :resources="resources" />
+        <button
+          @click="addResource"
+          class="btn btn-sm btn-primary">Add Resource</button>
       </div>
       <div class="col-md-8 order-md-1">
         <h4 class="mb-3">Resource
@@ -80,6 +83,20 @@
       },
       toggleView() {
         this.isDetailView = !this.isDetailView
+      },
+      addResource() {
+        const rnd = Math.random();
+        const _id = '_' + rnd.toString(36).slice(2)
+        const type = ['book', 'blog', 'video'][Math.floor(rnd * 3)]
+        const newResource = {
+          _id,
+          title: `Resource ${_id} Title`,
+          description: `Resource ${_id} Description`,
+          link: ``,
+          type
+        }
+
+        this.resources.unshift(newResource);
       }
     }
   }
