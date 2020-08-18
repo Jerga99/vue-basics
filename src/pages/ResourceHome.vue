6a0a0d2f7d14a774bddb6d7775c0cb54e15c0e25
Filip Jerga <jerga99@gmail.com>
2020-08-18 09:16:50 +0200

    Update view

--- src/pages/ResourceHome.vue
+++ src/pages/ResourceHome.vue
@@ -106,9 +106,10 @@
         // TODO: it's copied by reference!!!!
         this.selectedResource = selectedResource
       },
-      hydrateResources(newResources) {
-        console.log('hydrating')
-        console.log(newResources)
+      hydrateResources(newResource) {
+        const index = this.resources.findIndex(r => r._id === newResource._id)
+        this.resources[index] = newResource
+        this.selectResource(newResource)
       }
     }
   }
